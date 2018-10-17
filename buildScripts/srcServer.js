bede47bb12664433109b2cacf35f83527a65743d
unknown <USER-Admin@CTW-00164.criticaltechworks.com>
2018-10-17 10:48:52 +0100

    added /users route

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -17,6 +17,13 @@ app.get('/', function(req, res){
   res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
+app.get('/users', function(req, res){
+  res.json([
+    {"id": 1,"firstName":"Bob","lastName":"Smith","email":"bob@gmail.com"},
+    {"id": 2,"fisrtName":"John","lastName":"Doe","email":"jd@gmail.com"}
+  ]);
+});
+
 app.listen(port, function(err){
   if(err){
     console.log(err);
